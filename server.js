12d4faf63288fed63741911c8db434af48db08ee
Mandy <mandysommerfield@gmail.com>
2019-03-18 11:38:20 -0400

    set up for heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+//Middleware
 app.use(express.json());
+app.use(express.static(__dirname + '/client/build/'));
+
+//Routes
 app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
